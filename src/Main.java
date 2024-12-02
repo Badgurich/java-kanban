aefc12dc6a472dd466e1eb4a2145a7bd25b85afc
pl_gurov <nikita.gurov@mvideo.ru>
2024-12-02 21:20:00 +0300

    Final project 6 - code style

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import ru.yandex.practicum.taskmanager.enums.Status;
-import ru.yandex.practicum.taskmanager.interfaces.HistoryManager;
 import ru.yandex.practicum.taskmanager.interfaces.TaskManager;
 import ru.yandex.practicum.taskmanager.tasktypes.Epic;
 import ru.yandex.practicum.taskmanager.tasktypes.Subtask;
 import ru.yandex.practicum.taskmanager.tasktypes.Task;
-import ru.yandex.practicum.taskmanager.util.InMemoryTaskManager;
 import ru.yandex.practicum.taskmanager.util.Managers;
 
 static void main(String[] args) {
